5e9ef2d2eeeae6e7a8dec6874a4486574af57129
swarajitghosh <swarajit.ghosh.1997@gmail.com>
2025-04-21 03:17:06 +0530

    initial commit

---
+++ src/main/java/dev/swarajit/myfirstsprinfapp/MyFirstAPI.java
@@ -0,0 +1,20 @@
+package dev.swarajit.myfirstsprinfapp;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyFirstAPI {
+
+    @RequestMapping("/hello")
+public String sayHelloToAPerson(){
+return "Hello";
+}
+    @RequestMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return " Hello " + name;
+    }
+
+
+}
---
+++ src/test/java/dev/swarajit/myfirstsprinfapp/MyFirstSprinfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.swarajit.myfirstsprinfapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSprinfAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
